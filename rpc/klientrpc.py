4ea8ccc274cf426f9ac923947f176f787c60b5d7
Szymon <kowal160395@gmail.com>
2017-01-24 15:45:28 +0100

    poprawki_rcp

--- rpc/klientrpc.py
+++ rpc/klientrpc.py
@@ -1,7 +1,7 @@
 import xmlrpclib
 
 imie = raw_input("Podaj imie: ")
-proxy = xmlrpclib.ServerProxy("http://localhost:8008/")
+proxy = xmlrpclib.ServerProxy("http://localhost:8008/", verbose=True)
 #print "czy 3 jest parzyste: %s" % str(proxy.podzielna(3, 2))
 print "Jaki czas: %s" % str(proxy.czas())
 print "%s" % str(proxy.przywitaj(imie))
